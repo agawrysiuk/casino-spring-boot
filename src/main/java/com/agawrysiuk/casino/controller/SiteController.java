a38703f82e973a7c1c3ad7d4c56c375a2dc0c25d
dlrzz <arkad.gawry@gmail.com>
2019-10-29 10:05:49 +0100

    Spring Boot and Thymeleaf set up

---
+++ src/main/java/com/agawrysiuk/casino/controller/SiteController.java
@@ -0,0 +1,9 @@
+package com.agawrysiuk.casino.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+
+@Slf4j
+@Controller
+public class SiteController {
+}
---
+++ src/main/java/com/agawrysiuk/casino/util/ViewNames.java
@@ -0,0 +1,9 @@
+package com.agawrysiuk.casino.util;
+
+public final class ViewNames {
+
+    public static final String HOME = "home";
+
+    private ViewNames() {
+    }
+}
