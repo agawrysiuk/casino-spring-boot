895681b785215362a850e4200778459b2e7e5803
dlrzz <arkad.gawry@gmail.com>
2019-11-07 19:27:14 +0100

    Added security dependency

--- src/main/java/com/agawrysiuk/casino/repo/CasinoUserRepository.java
+++ src/main/java/com/agawrysiuk/casino/repo/CasinoUserRepository.java
@@ -2,9 +2,7 @@ package com.agawrysiuk.casino.repo;
 
 import com.agawrysiuk.casino.model.accounts.CasinoUser;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface CasinoUserRepository extends JpaRepository<CasinoUser,Integer> {
 
 }
