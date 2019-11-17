40a353fa03bf4a8c9b07192d1de60d0c0e72ae7b
dlrzz <arkad.gawry@gmail.com>
2019-11-17 13:49:51 +0100

    Added validator to CreditCardObject

---
+++ src/main/java/com/agawrysiuk/casino/model/database/CreditCardObject.java
@@ -0,0 +1,30 @@
+package com.agawrysiuk.casino.model.database;
+
+import com.agawrysiuk.casino.model.database.validator.ValidCreditCard;
+import lombok.Data;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+@Data
+@ValidCreditCard
+public class CreditCardObject {
+    @NotNull
+    @NotEmpty
+    private String firstName;
+    @NotNull
+    @NotEmpty
+    private String surname;
+    @NotNull
+    @NotEmpty
+    private String cardNumber;
+    @NotNull
+    @NotEmpty
+    private String expiryDate;
+    @NotNull
+    @NotEmpty
+    private String CCV;
+    @NotNull
+    @NotEmpty
+    private int depositAmount;
+}
