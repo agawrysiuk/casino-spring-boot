a6da215431a5cfc1d90e3fd90fa5b44a7be269d9
dlrzz <arkad.gawry@gmail.com>
2020-01-06 15:41:10 +0100

    Improved registerAccount code

--- src/main/java/com/agawrysiuk/casino/controller/RegisterController.java
+++ src/main/java/com/agawrysiuk/casino/controller/RegisterController.java
@@ -42,26 +42,26 @@ public class RegisterController {
             Errors errors) {
 
         log.info("showRegistrationForm() started");
-        if (!result.hasErrors()) {
-            log.info("showRegistrationForm() ---- NO ERRORS");
-            if (userService.findUserByEmail(userDto.getEmail()) != null) {
-                result.addError(new FieldError("userDto", "email", "Email already exists."));
-                log.info("showRegistrationForm() ---- EMAIL EXISTS");
-            }
-            if (userService.findUserByUsername(userDto.getUsername()) != null) {
-                result.addError(new FieldError("userDto", "username", "Username already exists."));
-                log.info("showRegistrationForm() ---- USERNAME EXISTS");
-            }
-        }
         if (result.hasErrors()) {
             log.info("showRegistrationForm() ---- ERROR {}", userDto);
             log.info("result = {}", result);
             return new ModelAndView(ViewNames.REGISTER, "userdto", userDto);
-        } else {
-            log.info("showRegistrationForm() ---- NO ERRORS {}", userDto);
-            userService.registerNewUserAccount(userDto);
-            return new ModelAndView(ViewNames.REGISTER_SUCCESS, "userdto", userDto);
         }
+        log.info("showRegistrationForm() ---- NO ERRORS");
+        if (userService.findUserByEmail(userDto.getEmail()) != null) {
+            result.addError(new FieldError("userDto", "email", "Email already exists."));
+            log.info("showRegistrationForm() ---- EMAIL EXISTS");
+            return new ModelAndView(ViewNames.REGISTER, "userdto", userDto);
+        }
+        if (userService.findUserByUsername(userDto.getUsername()) != null) {
+            result.addError(new FieldError("userDto", "username", "Username already exists."));
+            log.info("showRegistrationForm() ---- USERNAME EXISTS");
+            return new ModelAndView(ViewNames.REGISTER, "userdto", userDto);
+        }
+        log.info("showRegistrationForm() ---- NO ERRORS {}", userDto);
+        userService.registerNewUserAccount(userDto);
+        return new ModelAndView(ViewNames.REGISTER_SUCCESS, "userdto", userDto);
+
     }
 
     @RequestMapping(ViewNames.REGISTER_SUCCESS)
